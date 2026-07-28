1c6f5ca187ef2546116a66696d1e67e9627f47c1
SZ1011h <saiszr1939@gmail.com>
2026-07-28 17:24:00 -0300

    Se añadio comunicacion con el backend para generar tarjetas a partir de los datos del servidor en el carrusel

--- frontend_oficial/components/car_target_carrousel.jsx
+++ frontend_oficial/components/car_target_carrousel.jsx
@@ -1,22 +1,41 @@
 'use client'
-import { useRef } from "react";
+import { useRef,useEffect, useState } from "react";
 import "./car_target_carrousel.css"
 
-function CarTargetComp(){
+function CarTargetComp({brand,model,year,price,urlImage}){
     return  <div className="car-target">
                 <div>
-                    <img    src="images/car-example.webp"
+                    <img    src={urlImage}
                             alt="carro-ejemplo" />
                     <button><svg xmlns="http://www.w3.org/2000/svg" height="30px" viewBox="0 -960 960 960" width="30px" fill="#ffffff"><path d="M440-440H200v-80h240v-240h80v240h240v80H520v240h-80v-240Z"/></svg></button>
                 </div>
                 
-                <p>Este es el famoso carro que uso Tony Pro</p>
+                <h2>{brand} {model}</h2>
+                <p>{year}</p>
+                <p>{price}</p>
             </div>
 }
 
 
 
+
+async function getFetchData(url){
+    
+    const rest = await fetch(url);
+    const data = await rest.json();
+    return data;
+}
+
 export function TargetsCarrouselComp(){
+    const [carTargetData, setCarTargetData] = useState([]);
+    useEffect (()=>{
+        async function loadFetch(){
+            const data = await getFetchData("http://localhost:5000/api/autos/obtenerAutos");        
+            setCarTargetData(data.data)
+        }
+        loadFetch()
+    },[])
+    
 
     const carouselRef = useRef(null);
 
@@ -35,22 +54,27 @@ export function TargetsCarrouselComp(){
     };
 
     return (
-        <>
+        <> 
             
                 <div className="targets-container">
                     <button className="button-car-carrousel" onClick={previous}>←</button>
                     <div className="targets-carrousel" ref={carouselRef}>
-                        <CarTargetComp/>
-                        <CarTargetComp/>
-                        <CarTargetComp/>
-                        <CarTargetComp/>
-                        <CarTargetComp/>
-                        <CarTargetComp/>
-                        <CarTargetComp/>
-                        <CarTargetComp/>
+                        {carTargetData.map((carInfo) =>(
+                            <CarTargetComp 
+                                        key={carInfo.id}
+                                        brand ={carInfo.marca}
+                                        model={carInfo.modelo}
+                                        year={carInfo.anio}
+                                        price={carInfo.precio}
+                                        urlImage={carInfo.urlImagen[0]}
+
+                                         />
+                        ))}
+                        
+                    
                     </div>
                      <button className="button-car-carrousel" onClick={next}>→</button>
-                </div>
+                </div> 
                 
 
            
