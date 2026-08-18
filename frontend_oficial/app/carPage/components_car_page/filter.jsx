07b3728f8d4cba16f2b999f757f095815c21f500
MarcosaDavd <marcosmatias086@gmail.com>
2026-08-18 12:08:43 -0300

    fix: carPage form, use Controller react-hoook-form for inputs

--- frontend_oficial/app/carPage/components_car_page/filter.jsx
+++ frontend_oficial/app/carPage/components_car_page/filter.jsx
@@ -1,5 +1,5 @@
     'use client'
-import { useForm } from "react-hook-form";
+import { useForm, Controller } from "react-hook-form";
 import "./filter.css"
 {/**
 
@@ -81,32 +81,29 @@ function SelectOption({register,options,name}){
     */}
 
 export function Filter() {
-    const { register, reset: rhfReset, getValues } = useForm({
+    const { control, reset: rhfReset } = useForm({
         defaultValues: {
             transmision: "Todos",
         },
     });
 
     function resetForm() {
-        // Mostrar valores internos antes del reset
-        console.log('>>> antes reset', getValues());
-
-        // Pasar explícitamente los valores a reset garantiza que el formulario
-        // vuelva a los valores esperados incluso si algo fuera del formulario
-        // interfiere con los defaultValues iniciales.
         rhfReset({ transmision: 'Todos' });
-
-        // Mostrar valores internos después del reset
-        console.log('<<< despues reset', getValues());
     }
 
     return (
         <form className="filter-container">
-            <select {...register('transmision')}>
-                <option value="Todos">Todos</option>
-                <option value="Automatico">Automatico</option>
-                <option value="Manual">Manual</option>
-            </select>
+            <Controller
+                name="transmision"
+                control={control}
+                render={({ field }) => (
+                    <select {...field}>
+                        <option value="Todos">Todos</option>
+                        <option value="Automatico">Automatico</option>
+                        <option value="Manual">Manual</option>
+                    </select>
+                )}
+            />
 
             <button type="button" onClick={resetForm}>
                 RESET
