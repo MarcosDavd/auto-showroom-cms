e8b538208550325cb049bbbea625c3f7bfddc84e
SZ1011h <saiszr1939@gmail.com>
2026-08-25 10:04:40 -0300

    aplicacion chica de estilos para responsive de car target carrusel, flex-shrink a 0 (antes de darme cuenta que debia crear otra rama, mala mia David, lo siento :C)

--- frontend_oficial/next.config.mjs
+++ frontend_oficial/next.config.mjs
@@ -2,6 +2,11 @@
 const nextConfig = {
   /* config options here */
   reactCompiler: true,
+
+  allowedDevOrigins: [
+    'hardiest-fabiola-reprimandingly.ngrok-free.dev'
+  ],
+
   turbopack: {
     root: import.meta.dirname,
   },
